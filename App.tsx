d35dc3349a95aca82520d49776c30d8b5e053ede
deepanshu verma <deepanshuverma186@gmail.com>
2024-09-03 22:41:41 -0700

    using map

--- App.tsx
+++ App.tsx
@@ -3,11 +3,20 @@ import React from 'react';
 import {Text, View, Button} from 'react-native';
 
 function App(): React.JSX.Element {
+  const name:string = 'deepanshu';
+  const myInterests:Array<string> = ['Programming','Designing','Editing'];
   return (
     <View>
-      <Text style={{fontSize: 25, textAlign: 'center'}}>First Program</Text>
-      <Text style={{fontSize: 25, textAlign: 'center'}}>First Program</Text>
-      <Button title='Press here' ></Button>
+      <Text style={{fontSize: 25, textAlign: 'center'}}>{name}</Text>
+      {
+        myInterests.map((item,i)=>{
+         return(
+          <Text key={i} >
+          {item}
+         </Text>
+         )
+        })
+      }
     </View>
   );
 }
