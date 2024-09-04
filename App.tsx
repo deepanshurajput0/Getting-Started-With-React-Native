a2d1050e08da1835fd377a98fae484cdde20c7e8
deepanshu verma <deepanshuverma186@gmail.com>
2024-09-03 22:29:28 -0700

    adding buttons & text

--- App.tsx
+++ App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
-import {
-  Text,
-  View,
-} from 'react-native';
-
+import {Text, View, Button} from 'react-native';
 
 function App(): React.JSX.Element {
-
   return (
-     <View>
-     <Text style={{fontSize:30, textAlign:'center'}} >My first app</Text>
-     </View>  
+    <View>
+      <Text style={{fontSize: 25, textAlign: 'center'}}>First Program</Text>
+      <Text style={{fontSize: 25, textAlign: 'center'}}>First Program</Text>
+      <Button title='Press here' ></Button>
+    </View>
   );
 }
-
 
 export default App;
