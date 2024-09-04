3fd8e19e582f5bf9d6d3f49287abedce466a6e85
deepanshu verma <deepanshuverma186@gmail.com>
2024-09-03 23:37:25 -0700

    using conditionals

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {Text, View, Button} from 'react-native';
 function App(): React.JSX.Element {
   const name:string = 'deepanshu';
   const myInterests:Array<string> = ['Programming','Designing','Editing'];
+  const age:number = 18;
   return (
     <View>
       <Text style={{fontSize: 25, textAlign: 'center'}}>{name}</Text>
@@ -17,6 +18,11 @@ function App(): React.JSX.Element {
          )
         })
       }
+      <Text style={{fontSize: 25, textAlign: 'center'}} >
+        {
+          age < 18 ? 'You ar child' : 'You can Vote'
+        }
+      </Text>
     </View>
   );
 }
---
+++ components/ButtonEv.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ButtonEv = () => {
+  return (
+    <View>
+      <Text>ButtonEv</Text>
+    </View>
+  )
+}
+
+export default ButtonEv;
