22d5203add70ac2fd02d91e81ff9490f9b23b840
deepanshu verma <deepanshuverma186@gmail.com>
2024-09-11 14:43:59 -0700

    use react-navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { StatusBar, Platform, View, Text } from 'react-native'
+import { StatusBar, Platform, View, Text, Button } from 'react-native'
 import { WebView } from 'react-native-webview'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+const Stack = createNativeStackNavigator()
 function App(): React.JSX.Element {
   // const name:string = 'deepanshu';
   // const myInterests:Array<string> = ['Programming','Designing','Editing'];
   // const age:number = 18;
   return (
-    <WebView source={{uri:'https://www.youtube.com'}} />
-
+  <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name='Login' component={Login} />
+    <Stack.Screen name='Home' component={Home} />
+       
+    </Stack.Navigator>
+  </NavigationContainer>
   );
+}
+
+const Home =()=>{
+  return(
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+      <Text>Home Screen </Text>
+    </View>
+  )
+}
+const Login =(props)=>{
+  return(
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+      <Text>Login Page </Text>
+      <Button title='Go to Home' onPress={()=>props.navigation.navigate('Home')} />
+    </View>
+  )
 }
 
 export default App;
