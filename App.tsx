3f9d1492df3385aaf99a7eaccc995070b41ea9e7
deepanshu verma <deepanshuverma186@gmail.com>
2024-09-11 15:11:15 -0700

    styling in screens

--- App.tsx
+++ App.tsx
@@ -1,38 +1,51 @@
 import React from 'react';
-import { StatusBar, Platform, View, Text, Button } from 'react-native'
-import { WebView } from 'react-native-webview'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-const Stack = createNativeStackNavigator()
+import {StatusBar, Platform, View, Text, Button} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 function App(): React.JSX.Element {
   // const name:string = 'deepanshu';
   // const myInterests:Array<string> = ['Programming','Designing','Editing'];
   // const age:number = 18;
   return (
-  <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen name='Login' component={Login} />
-    <Stack.Screen name='Home' component={Home} />
-       
-    </Stack.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{title: 'User Login',
+            headerStyle:{
+              backgroundColor:'blue',
+            },
+            headerTitleStyle:{
+              color:'white'
+            }
+          }}
+        />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const Home =()=>{
-  return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+const Home = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home Screen </Text>
     </View>
-  )
-}
-const Login =(props)=>{
-  return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+  );
+};
+const Login = props => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Login Page </Text>
-      <Button title='Go to Home' onPress={()=>props.navigation.navigate('Home')} />
+      <Button
+        title="Go to Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default App;
